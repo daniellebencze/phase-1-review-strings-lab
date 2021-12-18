96f3188207cfdb600b660a3a6ff5b8b56610b9c7
Danielle Bencze <daniellebencze@live.com>
2021-12-18 12:51:47 -0800

    commit tree 78519cb158f47e48c81358017bb0bbdcdbb77863

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-// Write your code in this file!
-const currentUser = 'Grace Hopper';
+// // Write your code in this file!
+// const currentUser = 'Grace Hopper';
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+// const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = `Welcome, ${currentUser[0]}!`;
+// const firstInitial = currentUser[0];
+
+const currentUser = "Grace Hopper";
+// const welcomeMessage = "Welcome to Flatbook, " + "Grace Hopper" + "!";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser[0]}!`;
-const firstInitial = currentUser[0];
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
